b0a158b4dc17280690dda633c38154552fd3c6e0
Xiaochuan Ma <mxcins@gmail.com>
2019-08-22 13:58:20 +0800

    eslint config && prettier

---
+++ src/prettier.ts
@@ -0,0 +1,21 @@
+export = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      file: 'document.ejs',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+}

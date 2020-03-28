5d395e00112b56ffc055fdba70946bc12cb1484f
Xiaochuan Ma <mxcins@gmail.com>
2020-03-28 19:48:55 +0800

    update eslint config

--- src/eslint.ts
+++ src/eslint.ts
@@ -2,19 +2,13 @@ module.exports = {
   extends: [
     'airbnb',
     'airbnb-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:jest/recommended',
-    require.resolve('eslint-config-prettier'),
-    require.resolve('eslint-config-prettier/react'),
-    require.resolve('eslint-config-prettier/@typescript-eslint'),
+    'prettier',
   ],
   plugins: [
     '@typescript-eslint',
     'eslint-comments',
     'jest',
-    'unicorn',
-    'react-hooks'
+    'prettier',
   ],
   env: {
     browser: true,
@@ -24,14 +18,20 @@ module.exports = {
     jest: true,
     jasmine: true
   },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   rules: {
-    'react/jsx-wrap-multilines': 0,
-    'react/prop-types': 0,
-    'react/forbid-prop-types': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'react/jsx-props-no-spreading': 0,
-    'generator-star-spacing': 0,
-    'function-paren-newline': 0,
+    'prettier/prettier': 2,
     'import/no-unresolved': 0,
     'import/order': 'warn',
     'import/no-extraneous-dependencies': [
@@ -49,62 +49,9 @@ module.exports = {
         ]
       }
     ],
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'linebreak-style': 0,
-    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    'no-prototype-builtins': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-default-export': [0, 'camel-case'],
-    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': 'off',
-    // Use function hoisting to improve code readability
-    'no-use-before-define': [
-      'error',
-      { functions: false, classes: true, variables: true }
-    ],
-    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    '@typescript-eslint/explicit-function-return-type': [
-      'off',
-      { allowTypedFunctionExpressions: true }
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      { functions: false, classes: true, variables: true, typedefs: true }
-    ],
-    '@typescript-eslint/no-var-requires': 0,
-
-    // Common abbreviations are known and readable
-    'unicorn/prevent-abbreviations': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/interface-name-prefix': 0,
-    'import/no-cycle': 0,
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-
-    // issue https://github.com/facebook/react/issues/15204
-    'react-hooks/exhaustive-deps': 'off', // Checks effect dependencies
-
-    // Conflict with prettier
-    'arrow-body-style': ['error', 'as-needed'],
-    'object-curly-newline': 0,
-    'implicit-arrow-linebreak': 0,
-    'operator-linebreak': 0,
-    'eslint-comments/no-unlimited-disable': 1,
-    'no-param-reassign': 1,
-    'space-before-function-paren': 0,
-
-    // 2020-03-24 13:10:06 for prettier
-    'arrow-parens': [1, 'as-needed'],
-
-    // 2019-08-22 16:10:55 add
-    'consistent-return': 0,
   },
   settings: {
-    // support import modules from TypeScript files in JavaScript files
     "import/resolver": { node: { extensions: [".js", ".jsx", ".ts", ".tsx"] } },
     polyfills: ["fetch", "Promise", "URL", "object-assign"]
-  }
+  },
 };

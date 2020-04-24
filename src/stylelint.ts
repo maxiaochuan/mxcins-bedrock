eb1a6a0421f518df7640925dba0282026d1d912a
Xiaochuan Ma <mxcins@gmail.com>
2020-04-24 16:41:57 +0800

    fixed: add dependency stylelint-prettier

--- src/stylelint.ts
+++ src/stylelint.ts
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     require.resolve('stylelint-order'),
     require.resolve('stylelint-declaration-block-no-ignored-properties'),
+    require.resolve('sytlelint-prettier'),
   ],
   rules: {
     'no-descending-specificity': null,
